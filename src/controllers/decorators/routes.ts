ee07958b9df31f5dace0f69655b7de593fa16c40
IamJosephDaramola <daramolaj@gmail.com>
2022-08-13 04:00:29 +0100

    express server built with ts

---
+++ src/controllers/decorators/routes.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from "express";
+import "reflect-metadata";
+import { Methods } from "./Methods";
+import { MetaDataKeys } from "./MetadataKeys";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
+
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: RouteHandlerDescriptor) {
+      Reflect.defineMetadata(MetaDataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetaDataKeys.method, method, target, key);
+    };
+  };
+}
+
+export const get = routeBinder(Methods.get);
+export const post = routeBinder(Methods.post);
+export const put = routeBinder(Methods.put);
+export const del = routeBinder(Methods.del);
+export const patch = routeBinder(Methods.patch);
